8e0c01a087c533336368d41ef076af9a4065f625
Thiago Banfi Credico <hbanfi3@hotmail.com>
2023-05-21 11:52:08 -0300

    fix: refactor campaing section timer calculation

--- sections/Campaing.tsx
+++ sections/Campaing.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "preact/hooks";
+import { useState, useEffect } from "preact/hooks";
 
 export interface Props {
   title: string;
@@ -6,7 +6,7 @@ export interface Props {
   minutes: string;
   seconds: string;
   textButton: string;
-  email: string;
+  mail: string;
 }
 
 export default function CampaignTimer({
@@ -15,15 +15,15 @@ export default function CampaignTimer({
   minutes,
   seconds,
   textButton,
-  email,
+  mail,
 }: Props) {
-  const totalSeconds = (Number(hours) * 3600) + (Number(minutes) * 60) +
-    Number(seconds);
-  const [remainingSeconds, setRemainingSeconds] = useState(totalSeconds);
+  const [remainingTime, setRemainingTime] = useState(
+    Number(hours) * 3600 + Number(minutes) * 60 + Number(seconds)
+  );
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setRemainingSeconds((prevSeconds) => prevSeconds - 1);
+      setRemainingTime((prevTime) => prevTime - 1);
     }, 1000);
 
     return () => {
@@ -31,15 +31,7 @@ export default function CampaignTimer({
     };
   }, []);
 
-  useEffect(() => {
-    if (remainingSeconds <= 0) {
-      // Executar ação quando o tempo restante for igual a zero
-      console.log("Tempo encerrado!");
-    }
-  }, [remainingSeconds]);
-
-  // Função auxiliar para formatar o tempo restante no formato "HH:MM:SS"
-  const formatTime = (time: number) => {
+  const formatTime = (time: number): string => {
     const hours = Math.floor(time / 3600);
     const minutes = Math.floor((time % 3600) / 60);
     const seconds = Math.floor(time % 60);
@@ -51,36 +43,42 @@ export default function CampaignTimer({
     return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
   };
 
+  useEffect(() => {
+    if (remainingTime <= 0) {
+      console.log("Tempo encerrado!");
+    }
+  }, [remainingTime]);
+
   return (
-    <div class="h-28 py-4 px-16 w-screen bg-accent flex flex-col justify-center items-center text-accent-content gap-4 md:flex-row md:gap-16">
-      <h2 class="text-sm leading-5 md:text-xl">{title}</h2>
-      <div class="flex gap-6 md:gap-16">
-        <div class="flex gap-2 items-center">
-          <div class="flex flex-col items-center">
-            <p class="text-xl leading-7">
-              {formatTime(remainingSeconds).split(":")[0]}
+    <div className="h-28 py-4 px-16 w-screen bg-accent flex flex-col justify-center items-center text-accent-content gap-4 md:flex-row md:gap-16">
+      <h2 className="text-sm leading-5 md:text-xl">{title}</h2>
+      <div className="flex gap-6 md:gap-16">
+        <div className="flex gap-2 items-center">
+          <div className="flex flex-col items-center">
+            <p className="text-xl leading-7">
+              {formatTime(remainingTime).split(":")[0]}
             </p>
-            <p class="text-xs leading-4">Hours</p>
+            <p className="text-xs leading-4">Hours</p>
           </div>
-          <p class="text-xl leading-7">:</p>
-          <div class="flex flex-col items-center">
-            <p class="text-xl leading-7">
-              {formatTime(remainingSeconds).split(":")[1]}
+          <p className="text-xl leading-7">:</p>
+          <div className="flex flex-col items-center">
+            <p className="text-xl leading-7">
+              {formatTime(remainingTime).split(":")[1]}
             </p>
-            <p class="text-xs leading-4">Minutes</p>
+            <p className="text-xs leading-4">Minutes</p>
           </div>
-          <p class="text-xl leading-7">:</p>
-          <div class="flex flex-col items-center">
-            <p class="text-xl leading-7">
-              {formatTime(remainingSeconds).split(":")[2]}
+          <p className="text-xl leading-7">:</p>
+          <div className="flex flex-col items-center">
+            <p className="text-xl leading-7">
+              {formatTime(remainingTime).split(":")[2]}
             </p>
-            <p class="text-xs leading-4">Seconds</p>
+            <p className="text-xs leading-4">Seconds</p>
           </div>
         </div>
-        <a href={`mailto:${email}`}>
-          <button class="flex items-center gap-2 py-2 px-3 justify-center bg-primary text-primary-content leading-6">
-            {textButton}
-          </button>
+        <a href={`mailto:${mail}`}>
+        <button className="flex items-center gap-2 py-2 px-3 justify-center bg-primary text-primary-content leading-6">
+          {textButton}
+        </button>
         </a>
       </div>
     </div>
