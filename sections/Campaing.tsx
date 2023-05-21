76abc5c9a202e8ab5fae31121107999d30b8c4d9
Thiago Banfi Credico <hbanfi3@hotmail.com>
2023-05-21 10:08:04 -0300

    fix: refactor campaing section

--- sections/Campaing.tsx
+++ sections/Campaing.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "preact/hooks";
+
 export interface Props {
   title: string;
   hours?: string;
@@ -6,32 +8,59 @@ export interface Props {
   textButton: string;
 }
 
-export default function CampaignTimer(
-  { title, hours = "00", minutes = "00", seconds, textButton }: Props,
-) {
+export default function CampaignTimer({
+  title,
+  hours = "00",
+  minutes = "00",
+  seconds,
+  textButton,
+}: Props) {
+  const [remainingSeconds, setRemainingSeconds] = useState(Number(seconds));
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setRemainingSeconds((prevSeconds) => prevSeconds - 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  // Função auxiliar para formatar o tempo restante no formato "HH:MM:SS"
+  const formatTime = (time: number): string => {
+    const hours = Math.floor(time / 3600);
+    const minutes = Math.floor((time % 3600) / 60);
+    const seconds = Math.floor(time % 60);
+
+    const formattedHours = hours.toString().padStart(2, "0");
+    const formattedMinutes = minutes.toString().padStart(2, "0");
+    const formattedSeconds = seconds.toString().padStart(2, "0");
+
+    return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
+  };
+
   return (
-    <div class="h-28 py-4 px-16 w-screen bg-accent flex flex-col justify-center
-        items-center text-accent-content gap-4 md:flex-row md:gap-16">
+    <div class="h-28 py-4 px-16 w-screen bg-accent flex flex-col justify-center items-center text-accent-content gap-4 md:flex-row md:gap-16">
       <h2 class="text-sm leading-5 md:text-xl">{title}</h2>
       <div class="flex gap-6 md:gap-16">
         <div class="flex gap-2 items-center">
           <div class="flex flex-col items-center">
-            <p class="text-xl leading-7">{hours}</p>
+            <p class="text-xl leading-7">{formatTime(remainingSeconds).split(":")[0]}</p>
             <p class="text-xs leading-4">Hours</p>
           </div>
           <p class="text-xl leading-7">:</p>
           <div class="flex flex-col items-center">
-            <p class="text-xl leading-7">{minutes}</p>
+            <p class="text-xl leading-7">{formatTime(remainingSeconds).split(":")[1]}</p>
             <p class="text-xs leading-4">Minutes</p>
           </div>
           <p class="text-xl leading-7">:</p>
           <div class="flex flex-col items-center">
-            <p class="text-xl leading-7">{seconds}</p>
+            <p class="text-xl leading-7">{formatTime(remainingSeconds).split(":")[2]}</p>
             <p class="text-xs leading-4">Seconds</p>
           </div>
         </div>
-        <button class="flex items-center gap-2 py-2 px-3 justify-center bg-primary
-            text-primary-content leading-6">
+        <button class="flex items-center gap-2 py-2 px-3 justify-center bg-primary text-primary-content leading-6">
           {textButton}
         </button>
       </div>
